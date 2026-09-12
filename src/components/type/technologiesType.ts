dc44ee9daf9347e8536c94e4a7e9092a3219401f
K.M. Mozaddedul Islam Brinto <mozaddedul.islam.brinto02@gmail.com>
2026-09-12 22:36:08 +0800

    update Technologies.tsx

--- src/components/type/technologiesType.ts
+++ src/components/type/technologiesType.ts
@@ -2,6 +2,7 @@ export interface TechnologyType {
     "id": string,
     "name": string,
     "category": string,
+    "description": string,
     "icon": string,
     "rating": number,
     "difficulty": string,
